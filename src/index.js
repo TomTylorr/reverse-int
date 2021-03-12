095c02ef2673fc778d08e701539ba61a1591e026
TomTylor <tomtylorr@gmail.com>
2021-03-12 21:12:59 +0300

    feat: complited test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+
+    if (n === undefined) {
+        return 0;
+    }
+
+
+    n = n + "";
+    let array = [];
+    array  = n.split('').reverse().join('');
+
+    return +array;
   
 }
+
+
